d92d0aa94f5929a1acd443398e61ecbcf6379588
Daniel Agustin Tradito <datradito@gmail.com>
2021-09-14 10:16:11 -0300

    feat: console.log to debug on aws

--- src/utils/checksumAddress.ts
+++ src/utils/checksumAddress.ts
@@ -1,6 +1,9 @@
 import { checkAddressChecksum, toChecksumAddress } from 'web3-utils'
 
-export const checksumAddress = (address: string): string => toChecksumAddress(address)
+export const checksumAddress = (address: string): string => {
+  console.log('checksumAddress: ', address)
+  return toChecksumAddress(address)
+}
 
 export const isChecksumAddress = (address?: string): boolean => {
   if (address) {
