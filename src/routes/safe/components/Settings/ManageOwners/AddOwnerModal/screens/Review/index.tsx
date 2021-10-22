4427da63bb11adecf9456ef12b929bfd417bd3fd
Diogo Soares <diogo.soares@gnosis.pm>
2021-10-22 13:01:17 +0100

    Surround the review of the tx data with a try...catch and a useful error message.

--- src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/Review/index.tsx
+++ src/routes/safe/components/Settings/ManageOwners/AddOwnerModal/screens/Review/index.tsx
@@ -24,7 +24,6 @@ import { OwnerValues } from '../..'
 import { styles } from './style'
 import { EditableTxParameters } from 'src/routes/safe/components/Transactions/helpers/EditableTxParameters'
 import { getSafeSDK } from 'src/logic/wallets/getWeb3'
-import { Errors, logError } from 'src/logic/exceptions/CodedException'
 
 export const ADD_OWNER_SUBMIT_BTN_TEST_ID = 'add-owner-submit-btn'
 
@@ -78,7 +77,7 @@ export const ReviewAddOwner = ({ onClickBack, onClose, onSubmit, values }: Revie
           setData(txData)
         }
       } catch (error) {
-        logError(Errors._608, error.message)
+        console.error('Error calculating ERC721 transfer data for adding a Safe owner', error.message)
       }
     }
     calculateAddOwnerData()
--- src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/screens/Review/index.tsx
+++ src/routes/safe/components/Settings/ManageOwners/RemoveOwnerModal/screens/Review/index.tsx
@@ -24,7 +24,6 @@ import { EditableTxParameters } from 'src/routes/safe/components/Transactions/he
 import { useEstimationStatus } from 'src/logic/hooks/useEstimationStatus'
 import { sameAddress } from 'src/logic/wallets/ethAddresses'
 import { getSafeSDK } from 'src/logic/wallets/getWeb3'
-import { Errors, logError } from 'src/logic/exceptions/CodedException'
 
 export const REMOVE_OWNER_REVIEW_BTN_TEST_ID = 'remove-owner-review-btn'
 
@@ -89,7 +88,7 @@ export const ReviewRemoveOwnerModal = ({
           setData(txData)
         }
       } catch (error) {
-        logError(Errors._609, error.message)
+        console.error('Error calculating ERC721 transfer data for removing a Safe owner', error.message)
       }
     }
     calculateRemoveOwnerData()
