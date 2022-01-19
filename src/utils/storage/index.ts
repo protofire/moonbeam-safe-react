9105e1ef6302dfe6cebf760a62b2145add713a86
Daniel Agustin Tradito <datradito@gmail.com>
2022-01-19 17:26:29 -0300

    minor mock for testing purpose

--- src/utils/storage/index.ts
+++ src/utils/storage/index.ts
@@ -13,6 +13,9 @@ const STORAGE_KEYS: Record<ChainId, string> = {
   [CHAIN_ID.ENERGY_WEB_CHAIN]: 'ENERGY_WEB_CHAIN',
   [CHAIN_ID.ARBITRUM]: 'ARBITRUM',
   [CHAIN_ID.VOLTA]: 'VOLTA',
+  [CHAIN_ID.MOONBEAM]: 'MOONBEAM',
+  [CHAIN_ID.MOONRIVER]: 'MOONRIVER',
+  [CHAIN_ID.MOONBASE]: 'MOONBASE',
 }
 
 export const storage = new Storage(window.localStorage, '')
