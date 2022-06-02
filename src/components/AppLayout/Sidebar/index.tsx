b0f330a344ccfd38841b210defb9bc2535d14102
BLAKPC\blak <zhiltsov.nick@gmail.com>
2022-06-02 18:36:58 +0300

    fix: velas branding&logo

--- src/components/AppLayout/Sidebar/index.tsx
+++ src/components/AppLayout/Sidebar/index.tsx
@@ -2,10 +2,11 @@ import { lazy, useMemo } from 'react'
 import styled from 'styled-components'
 import { Divider } from '@gnosis.pm/safe-react-components'
 
-import List, { ListItemType } from 'src/components/List'
+import List, { ListItemType, StyledListItemText } from 'src/components/List'
 import SafeHeader from './SafeHeader'
 import { IS_PRODUCTION } from 'src/utils/constants'
 import { wrapInSuspense } from 'src/utils/wrapInSuspense'
+import ListIcon from 'src/components/List/ListIcon'
 import { background } from 'src/theme/variables'
 
 const StyledDivider = styled(Divider)`
@@ -15,6 +16,37 @@ const StyledDivider = styled(Divider)`
 
 const HelpContainer = styled.div`
   margin-top: auto;
+`
+
+const HelpList = styled.div`
+  margin: 24px 0;
+`
+
+const HelpCenterLink = styled.a`
+  width: 100%;
+  display: flex;
+  position: relative;
+  box-sizing: border-box;
+  text-align: left;
+  align-items: center;
+  padding: 8px 16px;
+  justify-content: flex-start;
+  text-decoration: none;
+  border-radius: 8px;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.background};
+  }
+  p {
+    font-family: ${({ theme }) => theme.fonts.fontFamily};
+    font-size: 0.76em;
+    font-weight: 600;
+    line-height: 1.5;
+    letter-spacing: 1px;
+    color: ${({ theme }) => theme.colors.placeHolder};
+    text-transform: uppercase;
+    padding: 0 0 0 4px;
+  }
 `
 
 type Props = {
@@ -70,6 +102,20 @@ const Sidebar = ({
         {debugToggle}
 
         <StyledDivider />
+
+        <HelpList>
+          {/* {!isDesktop && BEAMER_ID && (
+            <StyledListItem id="whats-new-button" button onClick={handleClick}>
+              <ListIcon type="gift" />
+              <StyledListItemText>What&apos;s new</StyledListItemText>
+            </StyledListItem>
+          )} */}
+
+          <HelpCenterLink href="https://support.velas.com/hc/en-150" target="_blank" title="Help Center of Velas Safe">
+            <ListIcon type="question" />
+            <StyledListItemText>Help Center</StyledListItemText>
+          </HelpCenterLink>
+        </HelpList>
       </HelpContainer>
     </>
   )
